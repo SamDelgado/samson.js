f6514b24406c927830ef627f4c6f98adb6376353
Sam Delgado <sam@samdelgado.com>
2015-06-22 16:58:01 -0700

    Added a default test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var async = require('../async-lite.js');
+var expect = require('expect.js');
+
+describe('samson', function() {
+
+  it('should pass since this is just a test', function(done) {
+
+    expect(true).to.be.ok();
+    done();
+
+  });
+
+});
