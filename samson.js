deb40cf4f51520c75fbdfd90b0ca3c307c91bd87
Sam Delgado <sam@samdelgado.com>
2015-08-10 09:17:43 -0700

    Update to the router events

--- samson.js
+++ samson.js
@@ -19,7 +19,7 @@ var reserved = ["$", "DOM", "Async", "Data", "styleSheet", "baseStyle", "style",
 // create the Samson object that will be exported
 module.exports = Samson = {};
 
-Samson.VERSION = '0.1.9'; // keep in sync with package.json
+Samson.VERSION = '0.1.10'; // keep in sync with package.json
 
 Samson.$ = $; // attach QuoJS to Samson
 
@@ -585,6 +585,9 @@ SamsonRouter.prototype._doFirst = function(name, callback) {
 SamsonRouter.prototype._duringAnimate = function() {
   var key;
   for (key in this.duringAnimate) {
+
+    console.log('duringAnimate key: ' + key);
+
     this.duringAnimate[key](this.getPageData());
   }
 };
